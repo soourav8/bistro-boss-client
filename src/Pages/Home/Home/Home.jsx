e5850445d574c96c38c539e9d2050b8257dd6c06
soourav8 <soourav8@gmail.com>
2023-06-01 01:05:06 +0530

    basic parallax done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Category from '../Category/Category';
-import ChefService from '../../../Components/ChefService/ChefService';
-import PopularMenu from '../PopularMenu/PopularMenu';
-import Featured from '../Featured/Featured';
-import Testimonials from '../Testimonials/Testimonials';
-import { Helmet} from 'react-helmet-async';
+import React from "react";
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+import ChefService from "../../../Components/ChefService/ChefService";
+import PopularMenu from "../PopularMenu/PopularMenu";
+import Featured from "../Featured/Featured";
+import Testimonials from "../Testimonials/Testimonials";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>Bistro Boss | Home</title>
-            </Helmet>
-            <Banner></Banner>
-            <Category></Category>
-            <ChefService></ChefService>
-            <PopularMenu></PopularMenu>
-            <Featured></Featured>
-            <Testimonials></Testimonials>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Home</title>
+      </Helmet>
+      <Banner></Banner>
+      <Category></Category>
+      <ChefService></ChefService>
+      <PopularMenu></PopularMenu>
+      <Featured></Featured>
+      <Testimonials></Testimonials>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Parallax, Background } from "react-parallax";
+
+const Cover = ({ img, title }) => {
+  return (
+    <div>
+      <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={img}
+        bgImageAlt="the menu"
+        strength={-200}
+      >
+        <div
+          className="hero h-[20rem] md:h-[40rem]"
+        //   style={{
+        //     backgroundImage: `url("${img}")`,
+        //   }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold uppercase">{title}</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Parallax>
+    </div>
+  );
+};
+
+export default Cover;
